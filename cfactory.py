44bc1decf22b46dbdd56b3987a14392b55396304
Shiv Shankar <shiv@pietisticmonk.com>
2010-07-18 21:18:36 -0700

    use memcache for caching results

--- cfactory.py
+++ cfactory.py
@@ -2,41 +2,55 @@
 # This is the factory to fetch the contents
 
 from entry import Entry
-import os, time
+import os, time, memcache
 from stat import *
 
+hostname = "%s:%s" % ("127.0.0.1", "11211")
 top = "articles"
+expiry = 180000
 
 class ContentFactory:
+    server = ''
+    
+    def __init__(self):
+        self.server = memcache.Client([hostname])
+
     def getEntry(self, file):
-        file = file + ".txt"
-        path = os.path.join(top, file)
+        e = self.server.get(file)
+        if not e:
+            file = file + ".txt"
+            path = os.path.join(top, file)
         
-        entry = open(path, 'r')
-        title = file.split(".")[0]
-        blob = entry.read()
-        url = 'article/' + title
-        date = Utils().getDate(os.stat(os.path.join(top, file))[ST_CTIME])
-        entry.close()
-        return Entry(date,title,url,blob)
+            entry = open(path, 'r')
+            title = file.split(".")[0]
+            blob = entry.read()
+            url = 'article/' + title
+            date = Utils().getDate(os.stat(os.path.join(top, file))[ST_CTIME])
+            entry.close()
+            e = Entry(date, title, url, blob)
+            self.server.set(file.split('.')[0], e, expiry)
+
+        return e
 
     def getArchive(self):
         """ This is under the assumption that none of you 
             use it the way its not supposed to be used.
         """
-
-        articles = []
-        for file in os.listdir(top):
-            if file.endswith(".txt"):
-                title = file.split(".")[0]
-                ctime = os.stat(os.path.join(top, file))[ST_CTIME]
-                sdate = Utils().getDate(ctime)
-                url = 'article/' + title
-                article = Entry(sdate, title, url)
-                article.ctime = ctime
-                articles.append(article)
-
-        articles.sort(key=lambda x: x.ctime, reverse=True)
+        articles = self.server.get("articleindex")
+        if not articles:
+            articles = []
+            for file in os.listdir(top):
+                if file.endswith(".txt"):
+                    title = file.split(".")[0]
+                    ctime = os.stat(os.path.join(top, file))[ST_CTIME]
+                    sdate = Utils().getDate(ctime)
+                    url = 'article/' + title
+                    article = Entry(sdate, title, url)
+                    article.ctime = ctime
+                    articles.append(article)
+            articles.sort(key=lambda x: x.ctime, reverse=True)
+            self.server.set("articleindex", articles, expiry)
+                    
         return articles
 
 class Utils:
